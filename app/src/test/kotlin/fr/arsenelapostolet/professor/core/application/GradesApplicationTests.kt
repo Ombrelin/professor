a33605507b0b191f16d7d9ef2cbf98eb1f5fbb2a
Arsène Lapostolet <arsene@lapostolet.fr>
2024-11-10 20:25:59 +0100

    port test

---
+++ app/src/test/kotlin/fr/arsenelapostolet/professor/core/application/GradesApplicationTests.kt
@@ -0,0 +1,44 @@
+package fr.arsenelapostolet.professor.core.application
+
+import fr.arsenelapostolet.professor.fakes.FakeStudentRepository
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class GradeApplicationTests {
+
+    private val studentRepository = FakeStudentRepository()
+    private val target = GradesApplication(studentRepository)
+
+    @Test
+    fun `when creating a new class, the csv is correctly parsed`() {
+        // Given
+        val csvLines = GradeApplicationTests::class.java.getResource("/students.csv")!!
+            .readText()
+            .lines()
+
+        // When
+        val result = target.createClass("lsi1", csvLines)
+
+        // Then
+        assertEquals("lsi1", result.name)
+        assertEquals(result.name, "lsi1")
+        assertEquals(result.students.size, csvLines.size - 1)
+        for (student in result.students) {
+            assertNotNull(student.id)
+            assertEquals(0, student.grades.size)
+            assertNotNull(student.firstName)
+            assertNotNull(student.lastName)
+            assertNotNull(student.email)
+            assertContains(student.email, "@")
+            assertNotNull(student.gitlabUsername)
+            assertNotNull(student.projectUrl)
+        }
+
+        //assertThat(result.students()).containsExactlyInAnyOrder(
+        //    studentRepository.getData().values().toArray(new Student [0])
+        //);
+    }
+
+}
